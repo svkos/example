0ba88bfa6f9458df5736c1eb07bb695cdc125258
svkos <sviridov@devite.ru>
2020-04-10 22:07:23 +0700

    Update WebmoneyAdapter.php

--- massPayment/methods/WebmoneyAdapter.php
+++ massPayment/methods/WebmoneyAdapter.php
@@ -12,14 +12,14 @@ class WebmoneyAdapter extends Payment implements PaymentInterface{
 		$wm->wmid = parent::getWmid();
 		$wm->folder = parent::getFolder();
 		$result = $wm->WMXML2( $this->transact_id,	//номер внутренней транзакции
-						  $this->lpurse,			//кошелек отправителя
-						  $this->rpurse,			//кошелек получателя
-						  $this->amount,			//сумма
-						  0,						//срок протекции в днях
-						  '',						//код протекции
-						  parent::getMessage(),		//примечание перевода
-						  0,						//номер счета в системе WebMoney, по которому выполняется перевод
-						  1);						//признак, который определяет, будет ли выполнен перевод, если получатель запрещает входящие переводы
+					$this->lpurse,		//кошелек отправителя
+					$this->rpurse,		//кошелек получателя
+					$this->amount,		//сумма
+					0,			//срок протекции в днях
+					'',			//код протекции
+					parent::getMessage(),	//примечание перевода
+					0,			//номер счета в системе WebMoney, по которому выполняется перевод
+					1);			//признак, который определяет, будет ли выполнен перевод, если получатель запрещает входящие переводы
 		
 		return $result['retval']==0 ? parent::afterPay() : "Код ошибки: ".$result['retval']."<br>Расшифровка - ".$result['retdesc'];
 	}
